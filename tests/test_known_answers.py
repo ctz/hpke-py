3ae2edfe0c37b7a404ec8aa39d5b98450df68395
Joseph Birr-Pixton <jpixton@gmail.com>
2022-04-09 14:40:49 +0100

    Format test code too

--- tests/test_known_answers.py
+++ tests/test_known_answers.py
@@ -9,7 +9,7 @@ import hpke
 
 @pytest.fixture
 def known_answers():
-    return json.load(open('tests/known_answers.json'))
+    return json.load(open("tests/known_answers.json"))
 
 
 def find_suite(kem_id, kdf_id, aead_id):
@@ -21,114 +21,122 @@ def find_suite(kem_id, kdf_id, aead_id):
         hpke.Suite__DHKEM_P256_HKDF_SHA256__HKDF_SHA512__ExportOnly,
     ]
     for suite in all:
-        if suite.KEM.ID == kem_id and suite.KDF.ID == kdf_id and suite.AEAD.ID == aead_id:
+        if (
+            suite.KEM.ID == kem_id
+            and suite.KDF.ID == kdf_id
+            and suite.AEAD.ID == aead_id
+        ):
             return suite
     return None
 
 
 def test_aeads(known_answers):
     for kat in known_answers:
-        suite = find_suite(kat['kem_id'], kat['kdf_id'], kat['aead_id'])
+        suite = find_suite(kat["kem_id"], kat["kdf_id"], kat["aead_id"])
         if suite is None:
             continue
 
-        for enc in kat['encryptions']:
-            c = suite.AEAD(bytes.fromhex(kat['key']),
-                           bytes.fromhex(enc['nonce']))
-            assert(c.seal(bytes.fromhex(enc['aad']),
-                          bytes.fromhex(enc['pt']))
-                   == bytes.fromhex(enc['ct']))
+        for enc in kat["encryptions"]:
+            c = suite.AEAD(bytes.fromhex(kat["key"]), bytes.fromhex(enc["nonce"]))
+            assert c.seal(
+                bytes.fromhex(enc["aad"]), bytes.fromhex(enc["pt"])
+            ) == bytes.fromhex(enc["ct"])
 
-        print('tested', suite.AEAD, 'OK')
+        print("tested", suite.AEAD, "OK")
+
 
 def test_receive_base_known_answer(known_answers):
     for kat in known_answers:
-        suite = find_suite(kat['kem_id'], kat['kdf_id'], kat['aead_id'])
-        mode = hpke.Mode(kat['mode'])
+        suite = find_suite(kat["kem_id"], kat["kdf_id"], kat["aead_id"])
+        mode = hpke.Mode(kat["mode"])
         if mode not in (hpke.Mode.BASE,) or suite is None:
             continue
 
-        print('testing', suite)
+        print("testing", suite)
         private_key = suite.KEM.decode_private_key(
-            bytes.fromhex(kat['skRm']),
-            bytes.fromhex(kat['pkRm']))
+            bytes.fromhex(kat["skRm"]), bytes.fromhex(kat["pkRm"])
+        )
 
         context = suite.setup_recv(
-            encap=bytes.fromhex(kat['pkEm']),
+            encap=bytes.fromhex(kat["pkEm"]),
             our_privatekey=private_key,
-            info=bytes.fromhex(kat['info']))
+            info=bytes.fromhex(kat["info"]),
+        )
 
-        for i, enc in enumerate(kat['encryptions']):
+        for i, enc in enumerate(kat["encryptions"]):
             if i == 0:
                 # test one-shot API
                 message = suite.open(
-                    bytes.fromhex(kat['pkEm']),
+                    bytes.fromhex(kat["pkEm"]),
                     private_key,
-                    info=bytes.fromhex(kat['info']),
-                    aad=bytes.fromhex(enc['aad']),
-                    ciphertext=bytes.fromhex(enc['ct']))
-                assert message == bytes.fromhex(enc['pt'])
-                print('  ', 'single-shot OK')
+                    info=bytes.fromhex(kat["info"]),
+                    aad=bytes.fromhex(enc["aad"]),
+                    ciphertext=bytes.fromhex(enc["ct"]),
+                )
+                assert message == bytes.fromhex(enc["pt"])
+                print("  ", "single-shot OK")
 
             message = context.aead.open(
-                    aad=bytes.fromhex(enc['aad']),
-                    ciphertext=bytes.fromhex(enc['ct']))
-            assert message == bytes.fromhex(enc['pt'])
-            print('  ', 'ciphertext', i, 'OK')
+                aad=bytes.fromhex(enc["aad"]), ciphertext=bytes.fromhex(enc["ct"])
+            )
+            assert message == bytes.fromhex(enc["pt"])
+            print("  ", "ciphertext", i, "OK")
 
-        for i, ex in enumerate(kat['exports']):
+        for i, ex in enumerate(kat["exports"]):
             got = context.export(
-                    exporter_context=bytes.fromhex(ex['exporter_context']),
-                    length=ex['L'])
-            assert got == bytes.fromhex(ex['exported_value'])
-            print('  ', 'exporter value', i, 'OK')
+                exporter_context=bytes.fromhex(ex["exporter_context"]), length=ex["L"]
+            )
+            assert got == bytes.fromhex(ex["exported_value"])
+            print("  ", "exporter value", i, "OK")
 
-        print('tested', suite, 'OK')
+        print("tested", suite, "OK")
+
 
 def test_receive_auth_known_answer(known_answers):
     for kat in known_answers:
-        suite = find_suite(kat['kem_id'], kat['kdf_id'], kat['aead_id'])
-        mode = hpke.Mode(kat['mode'])
+        suite = find_suite(kat["kem_id"], kat["kdf_id"], kat["aead_id"])
+        mode = hpke.Mode(kat["mode"])
         if mode not in (hpke.Mode.AUTH,) or suite is None:
             continue
 
-        print('testing', suite)
+        print("testing", suite)
         private_key = suite.KEM.decode_private_key(
-            bytes.fromhex(kat['skRm']),
-            bytes.fromhex(kat['pkRm']))
-        sender_pubkey = suite.KEM.decode_public_key(
-                bytes.fromhex(kat['pkSm']))
+            bytes.fromhex(kat["skRm"]), bytes.fromhex(kat["pkRm"])
+        )
+        sender_pubkey = suite.KEM.decode_public_key(bytes.fromhex(kat["pkSm"]))
 
         context = suite.setup_auth_recv(
-            encap=bytes.fromhex(kat['pkEm']),
+            encap=bytes.fromhex(kat["pkEm"]),
             our_privatekey=private_key,
-            info=bytes.fromhex(kat['info']),
-            peer_pubkey=sender_pubkey)
+            info=bytes.fromhex(kat["info"]),
+            peer_pubkey=sender_pubkey,
+        )
 
-        for i, enc in enumerate(kat['encryptions']):
+        for i, enc in enumerate(kat["encryptions"]):
             if i == 0:
                 # test one-shot API
                 message = suite.open_auth(
-                    bytes.fromhex(kat['pkEm']),
+                    bytes.fromhex(kat["pkEm"]),
                     private_key,
                     sender_pubkey,
-                    info=bytes.fromhex(kat['info']),
-                    aad=bytes.fromhex(enc['aad']),
-                    ciphertext=bytes.fromhex(enc['ct']))
-                assert message == bytes.fromhex(enc['pt'])
-                print('  ', 'single-shot OK')
+                    info=bytes.fromhex(kat["info"]),
+                    aad=bytes.fromhex(enc["aad"]),
+                    ciphertext=bytes.fromhex(enc["ct"]),
+                )
+                assert message == bytes.fromhex(enc["pt"])
+                print("  ", "single-shot OK")
 
             message = context.aead.open(
-                    aad=bytes.fromhex(enc['aad']),
-                    ciphertext=bytes.fromhex(enc['ct']))
-            assert message == bytes.fromhex(enc['pt'])
-            print('  ', 'ciphertext', i, 'OK')
+                aad=bytes.fromhex(enc["aad"]), ciphertext=bytes.fromhex(enc["ct"])
+            )
+            assert message == bytes.fromhex(enc["pt"])
+            print("  ", "ciphertext", i, "OK")
 
-        for i, ex in enumerate(kat['exports']):
+        for i, ex in enumerate(kat["exports"]):
             got = context.export(
-                    exporter_context=bytes.fromhex(ex['exporter_context']),
-                    length=ex['L'])
-            assert got == bytes.fromhex(ex['exported_value'])
-            print('  ', 'exporter value', i, 'OK')
+                exporter_context=bytes.fromhex(ex["exporter_context"]), length=ex["L"]
+            )
+            assert got == bytes.fromhex(ex["exported_value"])
+            print("  ", "exporter value", i, "OK")
 
-        print('tested', suite, 'OK')
+        print("tested", suite, "OK")
--- tests/test_pairwise.py
+++ tests/test_pairwise.py
@@ -14,6 +14,7 @@ def aead_suites():
         hpke.Suite__DHKEM_P521_HKDF_SHA512__HKDF_SHA512__AES_256_GCM,
     ]
 
+
 @pytest.fixture
 def export_suites():
     return [
@@ -21,31 +22,21 @@ def export_suites():
         hpke.Suite__DHKEM_P256_HKDF_SHA256__HKDF_SHA512__ExportOnly,
     ]
 
+
 def test_base_oneshot_pairwise(aead_suites):
     for suite in aead_suites:
         receiver = suite.KEM.generate_private_key()
         encap, ct = suite.seal(
-                receiver.public_key(),
-                info=b'info',
-                aad=b'aad',
-                message=b'message')
-        message = suite.open(
-                encap,
-                receiver,
-                info=b'info',
-                aad=b'aad',
-                ciphertext=ct)
-        assert message == b'message'
+            receiver.public_key(), info=b"info", aad=b"aad", message=b"message"
+        )
+        message = suite.open(encap, receiver, info=b"info", aad=b"aad", ciphertext=ct)
+        assert message == b"message"
 
         with pytest.raises(cryptography.exceptions.InvalidTag):
-            suite.open(
-                    encap,
-                    receiver,
-                    info=b'info',
-                    aad=b'aad',
-                    ciphertext=ct + b'eh')
+            suite.open(encap, receiver, info=b"info", aad=b"aad", ciphertext=ct + b"eh")
 
-        print(suite, 'OK')
+        print(suite, "OK")
+
 
 def test_auth_oneshot_pairwise(aead_suites):
     for suite in aead_suites:
@@ -53,54 +44,49 @@ def test_auth_oneshot_pairwise(aead_suites):
         sender = suite.KEM.generate_private_key()
 
         encap, ct = suite.seal_auth(
-                receiver.public_key(),
-                sender,
-                info=b'info',
-                aad=b'aad',
-                message=b'message')
+            receiver.public_key(), sender, info=b"info", aad=b"aad", message=b"message"
+        )
         message = suite.open_auth(
+            encap,
+            receiver,
+            sender.public_key(),
+            info=b"info",
+            aad=b"aad",
+            ciphertext=ct,
+        )
+        assert message == b"message"
+
+        with pytest.raises(cryptography.exceptions.InvalidTag):
+            suite.open_auth(
                 encap,
                 receiver,
                 sender.public_key(),
-                info=b'info',
-                aad=b'aad',
-                ciphertext=ct)
-        assert message == b'message'
+                info=b"info",
+                aad=b"aad",
+                ciphertext=ct + b"eh",
+            )
 
         with pytest.raises(cryptography.exceptions.InvalidTag):
             suite.open_auth(
-                    encap,
-                    receiver,
-                    sender.public_key(),
-                    info=b'info',
-                    aad=b'aad',
-                    ciphertext=ct + b'eh')
+                encap,
+                receiver,
+                receiver.public_key(),  # wrong
+                info=b"info",
+                aad=b"aad",
+                ciphertext=ct,
+            )
 
-        with pytest.raises(cryptography.exceptions.InvalidTag):
-            suite.open_auth(
-                    encap,
-                    receiver,
-                    receiver.public_key(), # wrong
-                    info=b'info',
-                    aad=b'aad',
-                    ciphertext=ct)
-
-        print(suite, 'OK')
+        print(suite, "OK")
 
 
 def test_base_pairwise(aead_suites):
     for suite in aead_suites:
         receiver = suite.KEM.generate_private_key()
-        encap, send_ctx = suite.setup_send(
-                receiver.public_key(),
-                info=b'info')
+        encap, send_ctx = suite.setup_send(receiver.public_key(), info=b"info")
 
-        recv_ctx = suite.setup_recv(
-                encap,
-                receiver,
-                info=b'info')
+        recv_ctx = suite.setup_recv(encap, receiver, info=b"info")
 
-        assert send_ctx.export(b'context', 512) == recv_ctx.export(b'context', 512)
+        assert send_ctx.export(b"context", 512) == recv_ctx.export(b"context", 512)
 
         for x in range(128):
             aad = os.urandom(32)
@@ -108,25 +94,19 @@ def test_base_pairwise(aead_suites):
             ct = send_ctx.aead.seal(aad=aad, message=msg)
             assert msg == recv_ctx.aead.open(aad=aad, ciphertext=ct)
 
-        print(suite, 'OK')
+        print(suite, "OK")
+
 
 def test_auth_pairwise(aead_suites):
     for suite in aead_suites:
         sender = suite.KEM.generate_private_key()
         receiver = suite.KEM.generate_private_key()
 
-        encap, send_ctx = suite.setup_auth_send(
-                receiver.public_key(),
-                b'info',
-                sender)
+        encap, send_ctx = suite.setup_auth_send(receiver.public_key(), b"info", sender)
 
-        recv_ctx = suite.setup_auth_recv(
-                encap,
-                receiver,
-                b'info',
-                sender.public_key())
+        recv_ctx = suite.setup_auth_recv(encap, receiver, b"info", sender.public_key())
 
-        assert send_ctx.export(b'context', 512) == recv_ctx.export(b'context', 512)
+        assert send_ctx.export(b"context", 512) == recv_ctx.export(b"context", 512)
 
         for x in range(128):
             aad = os.urandom(32)
@@ -134,49 +114,38 @@ def test_auth_pairwise(aead_suites):
             ct = send_ctx.aead.seal(aad=aad, message=msg)
             assert msg == recv_ctx.aead.open(aad=aad, ciphertext=ct)
 
-        print(suite, 'OK')
+        print(suite, "OK")
 
 
 def test_base_pairwise_export(export_suites):
     for suite in export_suites:
         receiver = suite.KEM.generate_private_key()
-        encap, send_ctx = suite.setup_send(
-                receiver.public_key(),
-                info=b'info')
+        encap, send_ctx = suite.setup_send(receiver.public_key(), info=b"info")
 
-        recv_ctx = suite.setup_recv(
-                encap,
-                receiver,
-                info=b'info')
+        recv_ctx = suite.setup_recv(encap, receiver, info=b"info")
 
-        assert send_ctx.export(b'context', 512) == recv_ctx.export(b'context', 512)
+        assert send_ctx.export(b"context", 512) == recv_ctx.export(b"context", 512)
 
         with pytest.raises(NotImplementedError):
-            send_ctx.aead.seal(aad=b'', message=b'')
+            send_ctx.aead.seal(aad=b"", message=b"")
 
         with pytest.raises(NotImplementedError):
-            recv_ctx.aead.open(aad=b'', ciphertext=b'')
+            recv_ctx.aead.open(aad=b"", ciphertext=b"")
+
 
 def test_auth_pairwise_export(export_suites):
     for suite in export_suites:
         sender = suite.KEM.generate_private_key()
         receiver = suite.KEM.generate_private_key()
 
-        encap, send_ctx = suite.setup_auth_send(
-                receiver.public_key(),
-                b'info',
-                sender)
+        encap, send_ctx = suite.setup_auth_send(receiver.public_key(), b"info", sender)
 
-        recv_ctx = suite.setup_auth_recv(
-                encap,
-                receiver,
-                b'info',
-                sender.public_key())
+        recv_ctx = suite.setup_auth_recv(encap, receiver, b"info", sender.public_key())
 
-        assert send_ctx.export(b'context', 512) == recv_ctx.export(b'context', 512)
+        assert send_ctx.export(b"context", 512) == recv_ctx.export(b"context", 512)
 
         with pytest.raises(NotImplementedError):
-            send_ctx.aead.seal(aad=b'', message=b'')
+            send_ctx.aead.seal(aad=b"", message=b"")
 
         with pytest.raises(NotImplementedError):
-            recv_ctx.aead.open(aad=b'', ciphertext=b'')
+            recv_ctx.aead.open(aad=b"", ciphertext=b"")
